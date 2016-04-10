6c39abd8621ab1af6d93cd49d42a5e5c51dac4d7
Douglas Moore <doug@dglmoore.com>
2016-04-10 12:23:29 -0700

    Setup build system and testing framework
    The project is using the CMake build-generation system to
    facilitate porting the project to other platforms.
    
    The testing framework is a modified version of CTest (see
    https://github.com/bvdberg/ctest.git).

---
+++ test/unittests/main.c
@@ -0,0 +1,11 @@
+// Copyright 2016 ELIFE. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+#define CTEST_MAIN
+
+#include <ctest.h>
+
+int main(int argc, char const** argv)
+{
+    return ctest_main(argc, argv);
+}
