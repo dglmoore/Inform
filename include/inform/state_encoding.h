35a0b28f9cc5984253cf19afab3a7c05da5e96e2
Douglas Moore <doug@dglmoore.com>
2016-04-11 22:16:45 -0700

    Add static linkage

--- include/inform/state_encoding.h
+++ include/inform/state_encoding.h
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 #define INFORM_ENCODING_ERROR(n) (inform_encoding_error + n)
-uint64_t const inform_encoding_error = 0x8000000000000000;
+static uint64_t const inform_encoding_error = 0x8000000000000000;
 
 uint64_t inform_encode(int *state, size_t n);
 int* inform_decode(uint64_t encoding, size_t n);
