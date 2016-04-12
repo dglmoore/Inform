b7cfa9fd4b789b02d856edb23f2ee4b76fb7aeb8
Douglas Moore <doug@dglmoore.com>
2016-04-11 22:48:12 -0700

    Create an "include all" header for the project

---
+++ include/inform/inform.h
@@ -0,0 +1,10 @@
+// Copyright 2016 ELIFE. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+#pragma once
+
+#include <inform/dist.h>
+#include <inform/entropy.h>
+#include <inform/state_encoding.h>
+#include <inform/time_series.h>
+
