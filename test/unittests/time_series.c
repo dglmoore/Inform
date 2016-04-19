4e723953020419f41b013888c5e68122a6285b73
Douglas Moore <doug@dglmoore.com>
2016-04-19 14:58:27 -0700

    Add timeseries tests for base 4 timeseries

--- test/unittests/time_series.c
+++ test/unittests/time_series.c
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 #include <ctest.h>
+#include <inform/state_encoding.h>
 #include <inform/time_series.h>
 #include <math.h>
 
@@ -20,7 +21,7 @@ CTEST(TimeSeries, ActiveInfoHistoryTooLong)
     ASSERT_FALSE(isnan(inform_active_info(series, 3, 2)));
 }
 
-CTEST(TimeSeries, ActiveInfoSingleSeries)
+CTEST(TimeSeries, ActiveInfoSingleSeries_Base2)
 {
     ASSERT_DBL_NEAR_TOL(0.918296,
             inform_active_info((int[]){1,1,0,0,1,0,0,1}, 8, 2),
@@ -63,6 +64,23 @@ CTEST(TimeSeries, ActiveInfoSingleSeries)
             1e-6);
 }
 
+#include <stdio.h>
+
+CTEST(TimeSeries, ActiveInfoSingleSeries_Base4)
+{
+    ASSERT_DBL_NEAR_TOL(1.270942,
+            inform_active_info((int[]){3,3,3,2,1,0,0,0,1}, 9, 2),
+            1e-6);
+
+    ASSERT_DBL_NEAR_TOL(1.270942,
+            inform_active_info((int[]){2,2,3,3,3,3,2,1,0}, 9, 2),
+            1e-6);
+
+    ASSERT_DBL_NEAR_TOL(0.469565,
+            inform_active_info((int[]){2,2,2,2,2,2,1,1,1}, 9, 2),
+            1e-6);
+}
+
 CTEST(TimeSeries, ActiveInfoEnsemble)
 {
     {
@@ -88,5 +106,19 @@ CTEST(TimeSeries, ActiveInfoEnsemble)
         };
         ASSERT_DBL_NEAR_TOL(0.3080467,
                 inform_active_info_ensemble(series, 9, 9, 2), 1e-6);
+    }
+}
+
+CTEST(TimeSeries, ActiveInfoEnsemble_Base4)
+{
+    {
+        int series[36] = {
+            3, 3, 3, 2, 1, 0, 0, 0, 1,
+            2, 2, 3, 3, 3, 3, 2, 1, 0,
+            0, 0, 0, 0, 1, 1, 0, 0, 0,
+            1, 1, 0, 0, 0, 1, 1, 2, 2,
+        };
+        ASSERT_DBL_NEAR_TOL(1.324291,
+                inform_active_info_ensemble(series, 4, 9, 2), 1e-6);
     }
 }
