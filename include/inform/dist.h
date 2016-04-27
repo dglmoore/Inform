82ff3cb06468bb2efafc51e8d243c80f9b648832
Douglas Moore <doug@dglmoore.com>
2016-04-26 18:26:08 -0700

    Document the distribution structure and associated functions

--- include/inform/dist.h
+++ include/inform/dist.h
@@ -7,22 +7,221 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/**
+ * A distribution of observed event frequencies
+ *
+ * This structure is the basis for almost all of the calculations performed
+ * via the library. It provides a means of managing observations and converting
+ * the observed event frequencies to probabilities.
+ *
+ * A distribution can be allocated with a set number of observable events
+ * (inform_dist_alloc), resized (inform_dist_realloc), copied 
+ * (inform_dist_copy) or duplicated (inform_dist_dup). Once the distribution
+ * has served its purpose, it can be freed (inform_dist_free).
+ *
+ * The distribution is, roughly, a histogram with finite support. The events
+ * are assumed to be mapped to the dense set of integers @f {0, 1, ..., N-1} @f
+ * where @f N @f is the number of observable events. The number of observable
+ * events can be extracted with inform_dist_size.
+ *
+ * Events can be observed one at a time (inform_dist_tick) or in batches
+ * (inform_dist_set). The occurance frequency of a given event can be
+ * obtained (inform_dist_get) as can the total number of observations
+ * (inform_dist_counts).
+ *
+ * Once the distribution populated, the probabilities can be extracted in either
+ * element-by-element (inform_dist_prob) or dumped to a dynamically allocated
+ * array (inform_dist_dump).
+ *
+ * Whenever the size of the support or the number of observed events is zero,
+ * the distrubution is considered invalid meaning that you can't trust any
+ * probabilities extracted from it. One can use inform_dist_is_valid to assess
+ * the validity of the distribution.
+ */
 typedef struct inform_distribution inform_dist;
 
+/**
+ * Allocate a distribution with a specified support size.
+ *
+ * The allocation will fail and return `NULL` if either `n == 0` or
+ * the memory allocation fails for whatever reason.
+ *
+ * Immediately following allocation, the distribution is invalid.
+ *
+ * @param[in] n the number of distinct events that could be observed
+ * @return the distribution
+ */
 inform_dist* inform_dist_alloc(size_t n);
+/**
+ * Resize the distribution to have new support.
+ *
+ * If the distribution is `NULL`, a new distribution of the requested
+ * size is allocated.
+ *
+ * If the distribution already has the requested size, then the original
+ * distribution is returned. If the requested size is greater than the
+ * current size, then the newly observable events are assigned
+ * zero frequency. If the requested size is less than the current size,
+ * then the discarded events are lost and the observation count is adjusted
+ * accordingly.
+ *
+ * Note that the distribution cannot be reduced to size `0`. In that case
+ * the distribution is left unmodified.
+ *
+ * In the event that the reallocation fails, the original distribution
+ * is left untouched.
+ *
+ * @param[in,out] dist the distribution to resize
+ * @param[in] n        the desired support size
+ * @return the reallocated distribution
+ */
 inform_dist* inform_dist_realloc(inform_dist *dist, size_t n);
+/**
+ * Copy a distribution to a destination.
+ *
+ * If the source distribution is `NULL`, then the destination is
+ * left untouched and `NULL` is returned.
+ *
+ * In the event that the destination is `NULL` or has a different
+ * size from that of the source, the destination is reallocated, and
+ * the result is returned.
+ *
+ * @param[in] src the source distribution
+ * @param[in,out] the destination distribution
+ * @return a pointer to the copied distribution
+ */
 inform_dist* inform_dist_copy(inform_dist const *src, inform_dist *dest);
+/**
+ * Duplicate a distribution.
+ *
+ * This function simply duplicates a distribution, essentially allocating
+ * a new distribution and copying the contents of the source to the new
+ * distribution.
+ *
+ * If the allocation fails or the source distribution is `NULL`, then the
+ * return value is `NULL`.
+ *
+ * @param[in] dist the source distribution
+ * @return the new distribution
+ */
 inform_dist* inform_dist_dup(inform_dist const *dist);
+/**
+ * Free all dynamically allocated memory associated with a distribution.
+ *
+ * @param[in] dist the distribution to free
+ */
 void inform_dist_free(inform_dist *dist);
 
+/**
+ * Get the size of the distribution's support.
+ *
+ * If the distribution is `NULL`, then a support of `0` is returned.
+ *
+ * @param[in] dist the distribution
+ * @return the size of the distribution's support
+ */
 size_t inform_dist_size(inform_dist const *dist);
+/**
+ * Get the total number of observations so far made.
+ *
+ * If the distribution is `NULL`, then return `0`.
+ *
+ * @param[in] dist the distribution
+ * @return the number of observations thus far made
+ */
 uint64_t inform_dist_counts(inform_dist const *dist);
+/**
+ * Determine whether or not the distribution is valid.
+ *
+ * In order to safely extract probabilities, the distribution must be
+ * non-`NULL`, the size of the support must be non-zero and the number
+ * of observations must be non-zero. In any other case, the distribution
+ * is invalid.
+ *
+ * @param[in] dist the distribution
+ * @return the validity of the distribution
+ */
 bool inform_dist_is_valid(inform_dist const *dist);
 
+/**
+ * Get the number of occurances of a given event.
+ *
+ * If the distribution is `NULL` or the `event` is not in the support, 
+ * `0` is returned.
+ *
+ * @param[in] dist  the distribution
+ * @param[in] event the event in question
+ * @return the number of observed occurances of the event
+ *
+ * @see inform_dist_set
+ */
 uint64_t inform_dist_get(inform_dist const *dist, uint64_t event);
+/**
+ * Set the number of occurances of a given event.
+ *
+ * This function manually sets the number of occurances of a given event.
+ * Note that the only restriction is that the value be positive. This means
+ * that this function can be used to invalidate the distribution by changing
+ * all of the event frequencies to zero.
+ *
+ * If the event is not in the support or the distribution is `NULL`, then
+ * nothing happens and zero is returned.
+ *
+ * @param[in,out] dist  the distribution
+ * @param[in] event     the event in question
+ * @return the new number of observed occurances of the event
+ *
+ * @see inform_dist_get
+ * @see inform_dist_tick
+ */
 uint64_t inform_dist_set(inform_dist *dist, uint64_t event, uint64_t x);
 
+/**
+ * Increment the number of observations of a given event.
+ *
+ * As an alternative to inform_dist_set, this function simply increments
+ * the number of occurances of a given event. This is useful for when
+ * iteratively observing events.
+ *
+ * If the event is not in the support or the distribution is `NULL`, then
+ * nothing happens and zero is returned.
+ *
+ * @param[in,out] dist the distribution
+ * @param[in] event    the event in question
+ * @return the new number of occurances of the event
+ *
+ * @see inform_dist_set
+ */
 uint64_t inform_dist_tick(inform_dist *dist, uint64_t event);
 
+/**
+ * Extact the probability of an event.
+ *
+ * This function simply computes the probability of a given event and
+ * returns that value.
+ * 
+ * If the event is not in the support, the distribution is `NULL`, or
+ * no observations have yet been made, then a zero probability is returned.
+ *
+ * @param[in] dist the distribution
+ * @param[in] event the event in question
+ * @return the heuristic probability of the event
+ *
+ * @see inform_dist_get
+ * @see inform_dist_dump
+ */
 double inform_dist_prob(inform_dist const *dist, uint64_t event);
+/**
+ * Dump the probabilities of all events to an array.
+ *
+ * This function computes the probabilities of all of the events in the
+ * support, stores them in a dynamically allocated array, and returns said
+ * array.
+ *
+ * If the distribution is `NULL`, the distribution has empty support, or the
+ * memory allocation fails, then a `NULL` array is returned.
+ *
+ * @param[in] dist the distribution
+ * @return a dynamically allocated array of the probabilities
+ */
 double* inform_dist_dump(inform_dist const *dist);
--- src/dist.c
+++ src/dist.c
@@ -7,107 +7,155 @@
 
 struct inform_distribution
 {
+    /// the size of the support
     size_t size;
+    /// the histogram or array of observation frequencies
     uint64_t *histogram;
+    /// the number of observations made so far
     uint64_t counts;
 };
 
 inform_dist* inform_dist_alloc(size_t n)
 {
+    // if the requested support size is zero, return NULL
     if (n == 0)
     {
         return NULL;
     }
+    // allocate the distribution
     inform_dist *dist = malloc(sizeof(inform_dist));
+    // if the allocation succeeded
     if (dist != NULL)
     {
+        // allocate the underlying histogram
         dist->histogram = calloc(n, sizeof(uint64_t));
+        // if the allocation succeeded
         if (dist->histogram != NULL)
         {
+            // set the distribution size and counts
             dist->size   = n;
             dist->counts = 0;
         }
+        // otherwise free the distribution
         else
         {
             free(dist);
+            dist = NULL;
         }
     }
+    // return the (potentially NULL) distribution
     return dist;
 }
 
 inform_dist* inform_dist_realloc(inform_dist *dist, size_t n)
 {
+    // if the requested size is zero, return the original distribution
+    if (n == 0)
+    {
+        return dist;
+    }
+    // if the distribution is not NULL and the requested size is different
+    // from the current size
     if (dist != NULL && dist->size != n)
     {
+        // realloc the histogram
         uint64_t *histogram = realloc(dist->histogram, n * sizeof(uint64_t));
+        // if the reallocation succeeded
         if (histogram != NULL)
         {
+            // reset the distribution's histogram to the new histogram
             dist->histogram = histogram;
+            // if the histogram was shrunken
             if (n < dist->size)
             {
+                // set the new distribution size
                 dist->size = n;
+                // sum up the counts that are in the smaller support
                 dist->counts = 0;
                 for (size_t i = 0; i < dist->size; ++i)
                 {
                     dist->counts += dist->histogram[i];
                 }
             }
+            // otherwise
             else
             {
+                // zero out all of the newly observable events
                 for (size_t i = dist->size; i < n; ++i)
                 {
                     dist->histogram[i] = 0;
                 }
+                // set the size of the distribution
                 dist->size = n;
             }
         }
+        // otherwise
         else
         {
+            // return NULL leaving the original distribution unscathed
             return NULL;
         }
     }
+    // if the original distribution was NULL
     else if (dist == NULL)
     {
+        // simply allocate the a new distribution of the requested size
         dist = inform_dist_alloc(n);
     }
+    // return the (potentially NULL) distribution
     return dist;
 }
 
 inform_dist* inform_dist_copy(inform_dist const *src, inform_dist *dest)
 {
+    // if the source is NULL, return NULL.
     if (src == NULL)
     {
         return NULL;
     }
+    // if the destination is NULL or the destination size is not the same
+    // as the source
     else if (dest == NULL || src->size != dest->size)
     {
-        inform_dist *redest = NULL;
-        if ((redest = inform_dist_realloc(dest, src->size)) == NULL)
+        // reallocate the destination
+        inform_dist *redest = inform_dist_realloc(dest, src->size);
+        // return NULL if the allocation fails
+        if (redest == NULL)
         {
             return NULL;
         }
+        // point dest at the new distribution
         dest = redest;
     }
+    // don't copy the source to itself
     else if (src == dest)
     {
         return dest;
     }
+    // copy the contents of the histogram from the source to the destination
     memcpy(src->histogram, dest->histogram, src->size * sizeof(uint64_t));
+    // set the counts appropriately
     dest->counts = src->counts;
+    // return the modified destination
     return dest;
 }
 
 inform_dist* inform_dist_dup(inform_dist const *dist)
 {
+    // if the source distribution is NULL, then just return NULL.
     if (dist == NULL)
     {
         return NULL;
     }
+    // allocate the new distribution
     inform_dist *dup = inform_dist_alloc(dist->size);
+    // if the allocation succeeded
     if (dup != NULL)
     {
+        // copy the source distribution to the new distribution
         inform_dist_copy(dist, dup);
     }
+    // return the (potentially NULL) distribution
     return dup;
 }
 
@@ -140,61 +188,81 @@ bool inform_dist_is_valid(inform_dist const *dist)
 
 uint64_t inform_dist_get(inform_dist const *dist, uint64_t event)
 {
+    // if the distribution is NULL or the event is outsize of the support
     if (dist == NULL || event >= dist->size)
     {
         return 0;
     }
+    // otherwise return the number of occurances of the event
     return dist->histogram[event];
 }
 
 uint64_t inform_dist_set(inform_dist *dist, uint64_t event, uint64_t x)
-{
+{    
+    // if the distribution is NULL or the event is outsize of the support
     if (dist == NULL || event >= dist->size)
     {
         return 0;
     }
+    // otherwise decrement counts by the old number of occurances
     dist->counts -= dist->histogram[event];
+    // increment counts by the new number of occurances
     dist->counts += x;
+    // set and return the new number of occurances of the event
     return (dist->histogram[event] = x);
 }
 
 uint64_t inform_dist_tick(inform_dist *dist, uint64_t event)
 {
+    // if the distribution is NULL or the event is outsize of the support
     if (dist == NULL || event >= dist->size)
     {
         return 0;
     }
+    // increment counts by one
     dist->counts += 1;
+    // increment by one and return the new number of occurances of the event
     return (dist->histogram[event] += 1);
 }
 
 static double inform_dist_unsafe_prob(inform_dist const *dist, uint64_t event)
 {
+    // unsafely compute the probability of an event
+    // this will fail if there have been no observations made
     return (double)(dist->histogram[event]) / dist->counts;
 }
 
 double inform_dist_prob(inform_dist const *dist, uint64_t event)
 {
-    if (dist == NULL || event >= dist->size)
+    // if the distribution is NULL, no observations have been made
+    // or the event is outsize of the support
+    if (dist == NULL || dist->counts == 0 || event >= dist->size)
     {
         return 0;
     }
+    // return the probability of the event
     return inform_dist_unsafe_prob(dist, event);
 }
 
 double* inform_dist_dump(inform_dist const *dist)
 {
+    // if the distribution is NULL or the event is outsize of the support
     if (dist == NULL || dist->size == 0)
     {
         return NULL;
     }
+    // allocate an array to store the probabilities
     double *probabilities = calloc(dist->size, sizeof(double));
+    // if the allocation succeeded
     if (probabilities != NULL)
     {
+        // loop over the events
         for (size_t i = 0; i < inform_dist_size(dist); ++i)
         {
+            // store their probabilities in the array
             probabilities[i] = inform_dist_unsafe_prob(dist,i);
         }
     }
+    // return the (possibly NULL) array of probabilities
     return probabilities;
 }
