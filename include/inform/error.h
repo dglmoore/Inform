dbb14d852827c589fc4dfeef34ba75f10feeab20
Douglas Moore <doug@dglmoore.com>
2016-07-16 12:15:11 -0700

    Stub out an error system

--- include/inform/error.h
+++ include/inform/error.h
@@ -52,6 +52,53 @@ double inform_nan(uint64_t tag);
  */
 uint64_t inform_nan_tag(double x);
 
+typedef enum
+{
+    INFORM_SUCCESS =  0,
+    INFORM_FAILURE = -1,
+    INFORM_EFAULT  =  1,
+    INFORM_EINVAL  =  2,
+    INFORM_ENOMEM  =  3,
+} inform_error_tag;
+
+typedef struct inform_error
+{
+    int errno;
+    char *msg;
+    int line;
+    char const *file;
+} inform_error;
+
+#define INFORM_ERROR_SUCCESS (inform_error) {\
+    .errno = INFORM_SUCCESS,\
+    .msg = NULL,\
+    .line = __LINE__,\
+    .file = __FILE__,}
+
+#define INFORM_ERROR(ERR, ERRNO, MSG) do {\
+        if ((ERR) != NULL) {\
+            *(ERR) = (inform_error) {\
+                .errno = (ERRNO),\
+                .msg = (MSG),\
+                .line = __LINE__,\
+                .file = __FILE__,\
+            };\
+        }\
+    } while(0)
+
+#define INFORM_ERROR_RETURN(ERR, ERRNO, MSG, RET) do {\
+        INFORM_ERROR(ERR, ERRNO, MSG); \
+        return RET; \
+    } while(0)
+
+#define INFORM_ERROR_RETURN_VOID(ERR, ERRNO, MSG) do {\
+        INFORM_ERROR(ERR, ERRNO, MSG); \
+        return; \
+    } while(0)
+
+#define INFORM_IS_SUCCESS(ERR) ((ERR) == NULL || (ERR)->errno == INFORM_SUCCESS)
+#define INFORM_IS_FAILURE(ERR) !INFORM_IS_SUCCESS(ERR)
+
 #ifdef __cplusplus
 }
 #endif
