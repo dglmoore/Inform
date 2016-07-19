8f657f4f7b7515964bb324d96e63d34aa039f380
Douglas Moore <doug@dglmoore.com>
2016-07-19 12:14:37 -0700

    Rename random.* to util.*

--- test/unittests/active_info.c
+++ test/unittests/active_info.c
@@ -1,7 +1,7 @@
 // Copyright 2016 ELIFE. All rights reserved.
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
-#include "random.h"
+#include "util.h"
 #include <inform/active_info.h>
 #include <math.h>
 #include <unit.h>
--- test/unittests/util.c
+++ test/unittests/util.c
@@ -1,7 +1,7 @@
 // Copyright 2016 ELIFE. All rights reserved.
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
-#include "random.h"
+#include "util.h"
 
 int* random_series(size_t size, int base)
 {
