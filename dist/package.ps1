e36d057f9b4a03dcf4bd3b9ae22bff5bbfbbb9c9
Douglas Moore <doug@dglmoore.com>
2016-07-20 20:28:58 -0400

    Create a packaging script for WIndows

---
+++ dist/package.ps1
@@ -0,0 +1,18 @@
+$version = (cmake . -Bbuild/production -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" | Select-String '-- inform version: ')
+$version = $version.ToString().Split(' ')[3]
+
+$prefix = "inform-$version"
+$target = "dist/$prefix"
+$archive = "$($target)_win-amd64.zip"
+
+cmake . -Bbuild/production -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$target" | Out-Null
+cd build/production
+nmake
+test/inform_unittest.exe
+nmake install
+cd ../..
+
+
+Add-Type -Assembly System.IO.Compression.FileSystem
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+[System.IO.Compression.ZipFile]::CreateFromDirectory($target, $archive, $compressionLevel, $false)
