5ced1e57c41d228f7a8aaccdaab03dbd4c346378
Douglas Moore <doug@dglmoore.com>
2016-08-01 10:53:26 -0700

    Modify the package.sh script to support OSX

--- dist/package.sh
+++ dist/package.sh
@@ -5,7 +5,6 @@ version=${version#"-- inform version: "}
 
 target=inform-$version
 prefix=dist/$target
-tarball=$target"_linux-x86_64.tar.gz"
 
 cmake -H. -Bbuild/production -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$prefix > /dev/null
 make -C build/production all test
@@ -13,6 +12,19 @@ make -C build/production install > /dev/null
 cp dist/install.sh $prefix > /dev/null
 cp LICENSE $prefix > /dev/null
 cp README.md $prefix > /dev/null
+
+system=`uname -s`
+hardware=`uname -m`
+
+if [[ "$system" == "Darwin" ]]; then
+    platform="macosx"
+elif [[ "$system" == "Linux" ]]; then
+    platform="linux"
+else
+    echo "unsupported platform: $system"
+fi
+
+tarball=$target"_"$platform"-"$hardware".tar.gz"
 
 cd dist
 echo "Creating tarball: dist/$tarball"
