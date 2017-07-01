0b6119708e4280062ca20143b600207a74c1b54e
Douglas Moore <doug@dglmoore.com>
2017-06-30 22:46:07 -0700

    Implement inform_effective_info

--- include/inform/effective_info.h
+++ include/inform/effective_info.h
@@ -10,6 +10,17 @@ extern "C"
 {
 #endif
 
+/**
+ * Compute the effective information of an intervention for a given transition
+ * probability matrix.
+ *
+ * If the provided intervention is @c NULL, the uniform distribution is assumed.
+ *
+ * @param[in] tpm   the transition probability matrix
+ * @param[in] inter the intervention distribution
+ * @param[in] n     the number of states in the system
+ * @return the effective information of the intervention
+ */
 EXPORT double inform_effective_info(double const *tpm, double const *inter,
     size_t n, inform_error *err);
 
