d6379d7a3c8eca53fe67bc2d46d2fe3918c33e83
Douglas Moore <doug@dglmoore.com>
2017-07-01 12:34:21 -0700

    Create unit tests for inform_cross_entropy

---
+++ src/cross_entropy.c
@@ -0,0 +1,11 @@
+// Copyright 2016-2017 ELIFE. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+#include <inform/cross_entropy.h>
+#include <inform/shannon.h>
+
+double inform_cross_entropy(int const *ps, int const *qs, size_t n, int b,
+    inform_error *err)
+{
+    return NAN;
+}
