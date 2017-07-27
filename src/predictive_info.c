4c43c420d2658d14ece157965a0b2c2e18ae68d9
Douglas Moore <doug@dglmoore.com>
2017-07-27 14:38:02 -0400

    Reduce PI calls to accumulate_observations

--- src/predictive_info.c
+++ src/predictive_info.c
@@ -4,37 +4,40 @@
 #include <inform/predictive_info.h>
 #include <inform/shannon.h>
 
-static void accumulate_observations(int const* series, size_t n, int b,
-    size_t kpast, size_t kfuture, inform_dist *states, inform_dist *histories,
-    inform_dist *futures)
+static void accumulate_observations(int const* series, size_t n, size_t m,
+    int b, size_t kpast, size_t kfuture, inform_dist *states,
+    inform_dist *histories, inform_dist *futures)
 {
-    int history = 0, q = 1, r = 1, state, future = 0;
-    for (size_t i = 0; i < kpast; ++i)
+    for (size_t i = 0; i < n; ++i, series += m)
     {
-        q *= b;
-        history *= b;
-        history += series[i];
+        int history = 0, q = 1, r = 1, state, future = 0;
+        for (size_t j = 0; j < kpast; ++j)
+        {
+            q *= b;
+            history *= b;
+            history += series[j];
+        }
+
+        for (size_t j = kpast; j < kpast + kfuture; ++j)
+        {
+            r *= b;
+            future *= b;
+            future += series[j];
+        }
+
+        size_t j = kpast + kfuture;
+        do
+        {
+            state = history * r + future;
+
+            states->histogram[state]++;
+            histories->histogram[history]++;
+            futures->histogram[future]++;
+
+            history = history * b - series[j - kpast - kfuture]*q + series[j - kfuture];
+            future = future * b - series[j - kfuture]*r + series[j];
+        } while (++j <= m);
     }
-
-    for (size_t i = kpast; i < kpast + kfuture; ++i)
-    {
-        r *= b;
-        future *= b;
-        future += series[i];
-    }
-
-    size_t i = kpast + kfuture;
-    do
-    {
-        state = history * r + future;
-
-        states->histogram[state]++;
-        histories->histogram[history]++;
-        futures->histogram[future]++;
-
-        history = history * b - series[i - kpast - kfuture]*q + series[i - kfuture];
-        future = future * b - series[i - kfuture]*r + series[i];
-    } while (++i <= n);
 }
 
 static void accumulate_local_observations(int const* series, size_t n, int b,
@@ -140,11 +143,8 @@ double inform_predictive_info(int const *series, size_t n, size_t m, int b,
     inform_dist histories = { data + states_size, histories_size, N };
     inform_dist futures   = { data + states_size + histories_size, futures_size, N };
 
-    for (size_t i = 0; i < n; ++i, series += m)
-    {
-        accumulate_observations(series, m, b, kpast, kfuture, &states,
-            &histories, &futures);
-    }
+    accumulate_observations(series, n, m, b, kpast, kfuture, &states,
+        &histories, &futures);
 
     double pi = inform_shannon_mi(&states, &histories, &futures, 2.0);
 
