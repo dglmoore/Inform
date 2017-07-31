db6a333ed0a1f2d94e409e336fbb2364abff6655
Douglas Moore <doug@dglmoore.com>
2017-07-30 17:31:40 -0700

    Remove the moving-window TE variant
    This is going to clean up the subsequent implementations.

--- include/inform/transfer_entropy.h
+++ include/inform/transfer_entropy.h
@@ -41,22 +41,6 @@ EXPORT double inform_transfer_entropy(int const *series_y, int const *series_x,
 EXPORT double *inform_local_transfer_entropy(int const *series_y, int const *series_x,
     size_t n, size_t m, int b, size_t k, double *te, inform_error *err);
 
-/**
- * Compute the local transfer entropy from one time series to another
- *
- * @param[in] series_y the ensemble of the source node
- * @param[in] series_x the ensemble of the target node
- * @param[in] n        the number initial conditions
- * @param[in] m        the number of time steps in each time series
- * @param[in] b        the base or number of distinct states at each time step
- * @param[in] k        the history length used to calculate the transfer entropy
- * @param[out] te      the transfer entropy
- * @param[out] err     an error structure
- * @return a pointer to the transfer entropy array
- */
-EXPORT double *inform_local_transfer_entropy2(int const *series_y, int const *series_x,
-    size_t n, size_t m, int b, size_t k, double *te, inform_error *err);
-
 #ifdef __cplusplus
 }
 #endif
